79a0fbfdbbc0c1a457063fad60c9a28c7aaab621
Nouk <clement.vannouque@gmail.com>
2019-02-13 19:36:19 +0100

    progress on the filters on execution report
    which is not filtering anything btw

--- src/app/layout/pages/reportbytag/reportbytag.component.ts
+++ src/app/layout/pages/reportbytag/reportbytag.component.ts
@@ -22,7 +22,12 @@ export class ReportbytagComponent implements OnInit {
   private selectedTag: ITag = null;
   // variables
   private countriesList: Array<IInvariant> = new Array();
+  private selected_countriesList: Array<string> = new Array();
   private tceStatusList: Array<IInvariant> = new Array();
+  private selected_tceStatusList: Array<string> = new Array();
+  private filtersShowed: boolean;
+  private showCountriesFilterOptions: boolean;
+  private showTceStatusFilterOptions: boolean;
   // DIRTY : need to create custom CSS class for earch TCE Status
   private TceStatus_Class: Array<any> = new Array(
     {
@@ -77,8 +82,19 @@ export class ReportbytagComponent implements OnInit {
     initChartJS();
     this.InvariantsService.getCountriesList();
     this.InvariantsService.getTceStatus();
-    this.InvariantsService.observableCountriesList.subscribe(response => { this.countriesList = response; });
-    this.InvariantsService.observableTceStatusList.subscribe(response => { this.tceStatusList = response; });
+    this.InvariantsService.observableCountriesList.subscribe(response => {
+      this.countriesList = response;
+      for (var index in this.countriesList) {
+        this.selected_countriesList[index] = this.countriesList[index].value;
+      }
+    });
+    this.InvariantsService.observableTceStatusList.subscribe(response => {
+      this.tceStatusList = response;
+      for (var index in this.tceStatusList) {
+        this.selected_tceStatusList[index] = this.tceStatusList[index].value;
+      }
+    });
+    this.filtersShowed = false;
   }
 
   onScrollToEnd() {
@@ -86,9 +102,8 @@ export class ReportbytagComponent implements OnInit {
   }
 
   selectedTagChange() {
-    if (this.selectedTag) {
-      this.router.navigate([], { queryParams: { tag: this.selectedTag.tag } });
-    }
+    if (this.selectedTag) { this.router.navigate([], { queryParams: { tag: this.selectedTag.tag } }); }
+    else { this.router.navigate([]); }
   }
 
   onScroll({ end }) {
@@ -107,8 +122,71 @@ export class ReportbytagComponent implements OnInit {
     }, 200)
   }
 
+  clearTag() {
+    this.selectedTag = null;
+    this.filtersShowed = false;
+    this.selectedTagChange();
+  }
+
   findStatusClass(status: string) {
     return this.TceStatus_Class.find(tces => tces.status === status).class;
   }
+
+  // countries global selection functions
+  isCountrySelected(country: string): boolean {
+    return this.selected_countriesList.filter(c => c === country).length > 0;
+  }
+  selectAllCountries() {
+    for (var index in this.countriesList) {
+      this.selected_countriesList[index] = this.countriesList[index].value;
+    }
+  }
+  UnselectAllCountries() {
+    this.selected_countriesList = [];
+  }
+  updateCountriesList($event, country: string) {
+    if (!$event.target.checked) {
+      // unchek : the country is removed
+      var index = this.selected_countriesList.indexOf(country);
+      this.selected_countriesList.splice(index, 1);
+    } else {
+      // check : the country is added
+      // if it is not already in the array
+      if (this.selected_countriesList.indexOf(country) == -1) {
+        this.selected_countriesList.push(country);
+      }
+    }
+  }
+
+  // status global selection functions
+  isTCEStatusSelected(tcestatus: string): boolean {
+    return this.selected_tceStatusList.filter(s => s === tcestatus).length > 0;
+  }
+  selectAllTCEStatus() {
+    for (var index in this.tceStatusList) {
+      this.selected_tceStatusList[index] = this.tceStatusList[index].value;
+    }
+  }
+  UnselectAllTCEStatus() {
+    this.selected_tceStatusList = [];
+  }
+  updateTCEStatusList($event, status: string) {
+    if (!$event.target.checked) {
+      // unchek : the status is removed
+      var index = this.selected_tceStatusList.indexOf(status);
+      this.selected_tceStatusList.splice(index, 1);
+    } else {
+      // check : the status is added
+      // if it is not already in the array
+      if (this.selected_tceStatusList.indexOf(status) == -1) {
+        this.selected_tceStatusList.push(status);
+      }
+    }
+  }
+
+  mouseEnterCountriesFilter() { this.showCountriesFilterOptions = true; }
+  mouseLeaveCountriesFilter() { this.showCountriesFilterOptions = false; }
+  mouseEnterTceStatusFilter() { this.showTceStatusFilterOptions = true; }
+  mouseLeaveTceStatusFilter() { this.showTceStatusFilterOptions = false; }
 
 }

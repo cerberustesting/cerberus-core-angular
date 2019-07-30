c705a2c8f2899b1d835af440397adc19e3495328
Anthony Quéré <47711333+Anthony-Jhoiro@users.noreply.github.com>
2019-07-30 13:00:39 +0200

    switch charts to ng2-charts

--- src/app/feat-analyse/reportbytag/graph/reportbystatus/reportbystatus.component.ts
+++ src/app/feat-analyse/reportbytag/graph/reportbystatus/reportbystatus.component.ts
@@ -21,33 +21,9 @@ export class ReportbystatusComponent implements OnInit {
     this.initChartJSLines();
   }
 
-  chartsParameters = {
-    labels: [
-      'Earnings',
-      'Sales',
-      'Tickets'
-    ],
-    datasets: {
-      data: [
-        65,
-        15,
-        20
-      ],
-      label: 'ygdrazil',
-      
-      backgroundColor: [
-        'rgba(141, 196, 81, 1)',
-        'rgba(255, 177, 25, 1)',
-        'rgba(224, 79, 26, 1)'
-      ],
-      hoverBackgroundColor: [
-        'rgba(141, 196, 81, .5)',
-        'rgba(255, 177, 25, .5)',
-        'rgba(224, 79, 26, .5)'
-      ]
-    }
-
-  };
+ labels: Label[];
+ colors: any;
+ data: number[];
 
 
   public initChartJSLines() {
@@ -71,52 +47,59 @@ export class ReportbystatusComponent implements OnInit {
         data: 30
       }
     ];
-    jQuery(() => {
-      // Get Chart Containers
-      let chartPieCon = jQuery("#" + this.graphID);
-      let chartPie;
-      // Set Chart and Chart Data variables
-      let chartPolarPieDonutData;
-      // Lines/Bar/Radar Chart Data
-      chartPolarPieDonutData = {
-        labels: [
-          'Earnings',
-          'Sales',
-          'Tickets'
-        ],
-        datasets: [{
-          data: [
-            65,
-            15,
-            20
-          ],
-          backgroundColor: [
-            'rgba(141, 196, 81, 1)',
-            'rgba(255, 177, 25, 1)',
-            'rgba(224, 79, 26, 1)'
-          ],
-          hoverBackgroundColor: [
-            'rgba(141, 196, 81, .5)',
-            'rgba(255, 177, 25, .5)',
-            'rgba(224, 79, 26, .5)'
-          ]
-        }]
-      };
-      // Init Charts
-      if (chartPieCon.length) {
-        //@ts-ignore
-        chartPie = new Chart(chartPieCon, {
-          type: 'pie', data: chartPolarPieDonutData, options: {
-            legend: {
-              display: false
-            },
-            tooltips: {
-              enabled: true
-            }
-          }
-        });
+    this.labels = compo.map(e => e.label);
+    this.colors = [
+      {
+        backgroundColor: compo.map(e => e.color)
       }
-    });
+    ];
+    this.data = compo.map(e => e.data);
+    // jQuery(() => {
+    //   // Get Chart Containers
+    //   let chartPieCon = jQuery("#" + this.graphID);
+    //   let chartPie;
+    //   // Set Chart and Chart Data variables
+    //   let chartPolarPieDonutData;
+    //   // Lines/Bar/Radar Chart Data
+    //   chartPolarPieDonutData = {
+    //     labels: [
+    //       'Earnings',
+    //       'Sales',
+    //       'Tickets'
+    //     ],
+    //     datasets: [{
+    //       data: [
+    //         65,
+    //         15,
+    //         20
+    //       ],
+    //       backgroundColor: [
+    //         'rgba(141, 196, 81, 1)',
+    //         'rgba(255, 177, 25, 1)',
+    //         'rgba(224, 79, 26, 1)'
+    //       ],
+    //       hoverBackgroundColor: [
+    //         'rgba(141, 196, 81, .5)',
+    //         'rgba(255, 177, 25, .5)',
+    //         'rgba(224, 79, 26, .5)'
+    //       ]
+    //     }]
+    //   };
+    //   // Init Charts
+    //   if (chartPieCon.length) {
+    //     //@ts-ignore
+    //     chartPie = new Chart(chartPieCon, {
+    //       type: 'pie', data: chartPolarPieDonutData, options: {
+    //         legend: {
+    //           display: false
+    //         },
+    //         tooltips: {
+    //           enabled: true
+    //         }
+    //       }
+    //     });
+    //   }
+    // });
   }
 
   ngOnInit() {

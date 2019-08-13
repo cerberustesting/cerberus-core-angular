9e13bc1ca9efdb5b590e9716f036f1191eef9e61
Anthony Quéré <47711333+Anthony-Jhoiro@users.noreply.github.com>
2019-08-13 10:00:49 +0200

    #8 Add country list with checkboxes

--- src/app/feat-design/testcasescript/tc-script/property-v2/property-v2.component.ts
+++ src/app/feat-design/testcasescript/tc-script/property-v2/property-v2.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { IProperty } from 'src/app/shared/model/property.model';
 import { InvariantsService } from 'src/app/core/services/crud/invariants.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ITestCaseHeader } from 'src/app/shared/model/testcase.model';
+import { TestService } from 'src/app/core/services/crud/test.service';
 
 
 @Component({
@@ -13,14 +15,14 @@ export class PropertyV2Component implements OnInit {
   @Input('properties') propertiesList: Array<any>;
   @ViewChild('myTable', { static: false }) table: any;
 
-  constructor(private invariantService: InvariantsService, private formBuilder: FormBuilder) { }
+  constructor(private invariantService: InvariantsService, private formBuilder: FormBuilder, private testService: TestService) { }
   properties = [
-    {name: 'name 1', properties: [{index: 1},{index: 2},{index: 3},{index: 4}]},
-    {name: 'name 2', properties: [{index: 1},{index: 2},{index: 3},{index: 4}]},
-    {name: 'name 3', properties: [{index: 1},{index: 2},{index: 3},{index: 4}]},
-    {name: 'name 4', properties: [{index: 1},{index: 2},{index: 3},{index: 4}]},
-    {name: 'name 5', properties: [{index: 1},{index: 2},{index: 3},{index: 4}]},
-    {name: 'name 6', properties: [{index: 1},{index: 2},{index: 3},{index: 4}]},
+    { name: 'name 1', properties: [{ index: 1 }, { index: 2 }, { index: 3 }, { index: 4 }] },
+    { name: 'name 2', properties: [{ index: 1 }, { index: 2 }, { index: 3 }, { index: 4 }] },
+    { name: 'name 3', properties: [{ index: 1 }, { index: 2 }, { index: 3 }, { index: 4 }] },
+    { name: 'name 4', properties: [{ index: 1 }, { index: 2 }, { index: 3 }, { index: 4 }] },
+    { name: 'name 5', properties: [{ index: 1 }, { index: 2 }, { index: 3 }, { index: 4 }] },
+    { name: 'name 6', properties: [{ index: 1 }, { index: 2 }, { index: 3 }, { index: 4 }] },
   ]
   mapProperties = [];
   editing: Array<boolean>;
@@ -43,22 +45,26 @@ export class PropertyV2Component implements OnInit {
   propertyType: Array<any>;
   propertyDatabase: Array<any>;
   propertyNature: Array<any>;
+  countryList: Array<any>;
+  private testcaseheader: ITestCaseHeader;
   onActivate(event) {
-    if(event.type == 'click') {
+    if (event.type == 'click') {
       this.table.rowDetail.toggleExpandRow(event.row);
     }
   }
 
   ngOnInit() {
+    this.testService.observableTestCase.subscribe(r => { this.testcaseheader = r.info; });
     this.propertyType = this.invariantService.propertyTypeList;
     this.propertyDatabase = this.invariantService.propertyDatabaseList;
     this.propertyNature = this.invariantService.propertyNatureList;
-    this.editing = this.propertiesList.map(e=>false);
+    this.countryList = this.invariantService.countriesList; // TODO : remove countries outside TC header
+    this.editing = this.propertiesList.map(e => false);
+    this.countryList = this.testService.convertCountriesList(this.testcaseheader);
     this.propertyForms = this.propertiesList.map(e => {
       return this.formBuilder.group({
         type: e.type,
         database: e.database,
-        countryList: e.countryList,
         value: e.value1,
         description: e.description,
         length: e.length,
@@ -68,16 +74,44 @@ export class PropertyV2Component implements OnInit {
       })
     });
     let temp = {};
+    let tempCountry = {};
+
+
+
     for (let property of this.propertiesList) {
-      if (!temp[property.property]) temp[property.property] = {properties: []};
+      if (!temp[property.property]) temp[property.property] = { properties: [], countries: [] };      
       temp[property.property].properties.push(property);
+      temp[property.property].countries.concat(property.country);
     }
     for (let property in temp) {
-      this.mapProperties.push({name: property, properties: temp[property].properties});
+      this.mapProperties.push({ name: property, properties: temp[property].properties, country: temp[property].countries.filter((item, index)=> 
+               temp[property].countries.indexOf(item)===index
+      )});
     }
   }
   updateValue(rowIndex) {
     this.editing[rowIndex] = false;
   }
+
+  toggleChk(property, country) {
+    if (property.country.includes(country)) property.country.splice(property.country.indexOf(country));
+    else property.country.push(country);
+  }
+
+  // getPropertyCountryList(property) {
+  //   let rep = this.countryList;
+  //   this.propertiesList.filter(p => p.property === property.property).forEach((p, i) => {
+        
+  //       p.country.forEach(c => {
+  //         if (rep.includes(c)) {
+  //           console.log("splice",c)
+  //           rep.splice(i,1)
+  //         }
+  //       })
+      
+  //   });
+    
+  //   return rep.concat(property.country).filter((c, i, list) => list.indexOf(c)===i);
+  // }
 
 }

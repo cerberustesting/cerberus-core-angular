e2c5c3254b4564ad5545401398c5c6a9aa5433f6
Nouxx <clement.vannouque@gmail.com>
2019-09-06 17:50:09 +0200

    Duplicate environment variables for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+// KEYCLOAK INFORMATION
+let keycloakConfig: any = {
+  url: 'https://auth.cerberus-testing.org/auth',
+  realm: 'CerberusQA',
+  clientId: 'cerberus-angular'
+};
+
+// CERBERUS API ENDPOINT
+let API_endpoint: string = "https://qa.cerberus-testing.org";
+
 export const environment = {
-  production: true
+  production: true,
+  cerberus_api_url: API_endpoint,
+  keycloak: keycloakConfig
 };

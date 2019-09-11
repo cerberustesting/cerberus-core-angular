439de7583d9af21d68ff748ca1f91a48de8e3235
Nouxx <clement.vannouque@gmail.com>
2019-09-11 11:21:22 +0200

    update deploy script
    script is creating a .zip archive (dynamic name) with the file to upload

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,5 +1,8 @@
+# get project version from package.json
+version=$(grep -oP '(?<=version": ")[^"]*' ./package.json)
+
 # zip the dist folder
-zip cerberus-front-0.1.zip -r ./dist
+zip cerberus-front-${version}.zip -r ./dist
 
 # move it to ./dist
-mv ./cerberus-front-0.1.zip ./dist
+mv ./cerberus-front-${version}.zip ./dist

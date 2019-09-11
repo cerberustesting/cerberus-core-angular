215a0e1827d5aa4fe243ca5c61601d5cf0d88c97
Nouxx <clement.vannouque@gmail.com>
2019-09-11 10:06:52 +0200

    change project name from "cerberus4" to "cerberus-front"

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to cerberus4!');
+    expect(page.getParagraphText()).toEqual('Welcome to cerberus-front!');
   });
 });

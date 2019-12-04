5706070e52508325191f6f4404f5755c74342e74
Nouxx <clement.vannouque@gmail.com>
2019-12-04 14:59:10 +0100

    front end design for datatable mass actions
    #9

--- src/app/feat-design/testcaselist/actions/actions.component.ts
+++ src/app/feat-design/testcaselist/actions/actions.component.ts
@@ -1,26 +1,42 @@
-import { Component, OnInit, NgModule, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RunComponent } from '../../../shared/run/run.component';
 import { SidecontentService } from '../../../core/services/crud/sidecontent.service';
-
 
 @Component({
   selector: 'app-actions',
   templateUrl: './actions.component.html',
   styleUrls: ['./actions.component.scss']
 })
-export class ActionsComponent implements OnInit {
+export class ActionsComponent {
+
+  // the array with all the selected rows
+  // the object type depends on the table (ex: test case)
   @Input() selectedRows: Array<any>;
 
-
   constructor(
-      private sideContentService: SidecontentService
+    private sideContentService: SidecontentService
   ) { }
 
-  ngOnInit() {
+  // return the selected rows to the view
+  // returns undefined if the array is empty
+  getSelection(): Array<any> {
+    if (this.selectedRows) {
+      if (this.selectedRows.length > 0) {
+        return this.selectedRows;
+      } else {
+        return undefined;
+      }
+    } else {
+      return undefined;
+    }
+  }
+
+  debug() {
+    console.log(this.selectedRows);
   }
 
   runTestCases(selectedRows) {
-    this.sideContentService.addComponentToSideBlock(RunComponent, {testCases: selectedRows});
+    this.sideContentService.addComponentToSideBlock(RunComponent, { testCases: selectedRows });
     this.sideContentService.openSideBlock();
   }
 }
